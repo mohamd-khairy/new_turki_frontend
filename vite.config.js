368fa723617995cf98db0acde1dc5a6641bcab08
Mohamed Khairy <mohamed.khairy.eg@gmail.com>
2024-11-25 12:02:09 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -13,10 +13,7 @@ import vuetify from 'vite-plugin-vuetify'
 // https://vitejs.dev/config/
 export default defineConfig({
   // base:"",
-
-  //
   // base:"/dashboard/",
-
   base:"/dashboard-v2/",
 
   // base:"/dashboard-v3/",
