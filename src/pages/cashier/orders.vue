154150efa9fa72be66075d313e9095bca8272aed
Mohamed Khairy <mohamed.khairy.eg@gmail.com>
2025-01-26 18:44:19 +0300

    fix

--- src/pages/cashier/orders.vue
+++ src/pages/cashier/orders.vue
@@ -367,7 +367,6 @@
                   </template>
                 </VTooltip>
                 <VTooltip
-                  v-if="order.paid != 1"
                   text=" تعديل الطلب"
                 >
                   <template #activator="{ props }">
@@ -386,37 +385,39 @@
                     </VBtn>
                   </template>
                 </VTooltip>
-                <VTooltip text="ارجاع الطلب">
+                <!--
+                  <VTooltip text="ارجاع الطلب">
                   <template #activator="{ props }">
-                    <VBtn
-                      v-bind="props"
-                      icon
-                      variant="plain"
-                      color="default"
-                      size="x-small"
-                      @click="openRefundDetails(order)"
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        width="24"
-                        height="24"
-                        stroke-width="2"
-                      >
-                        <path d="M12 19h-6a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v4.5" />
-                        <path d="M3 10h18" />
-                        <path d="M7 15h.01" />
-                        <path d="M11 15h2" />
-                        <path d="M16 19h6" />
-                        <path d="M19 16l-3 3l3 3" />
-                      </svg>
-                    </VBtn>
+                  <VBtn
+                  v-bind="props"
+                  icon
+                  variant="plain"
+                  color="default"
+                  size="x-small"
+                  @click="openRefundDetails(order)"
+                  >
+                  <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  width="24"
+                  height="24"
+                  stroke-width="2"
+                  >
+                  <path d="M12 19h-6a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v4.5" />
+                  <path d="M3 10h18" />
+                  <path d="M7 15h.01" />
+                  <path d="M11 15h2" />
+                  <path d="M16 19h6" />
+                  <path d="M19 16l-3 3l3 3" />
+                  </svg>
+                  </VBtn>
                   </template>
-                </VTooltip>
+                  </VTooltip>
+                -->
                 <VTooltip
                   v-if="order.paid != 1 && hasRole('cashier-manager')"
                   text="خصم إضافي للعميل"
